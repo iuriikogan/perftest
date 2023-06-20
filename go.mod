0da5912cbe099d5af3b649c4aad87e911596db33
Iurii Kogan <koganiurii@gmail.com>
2023-06-20 16:07:59 +0300

    Organize Imports

--- go.mod
+++ go.mod
@@ -6,14 +6,7 @@ require (
 	github.com/aws/constructs-go/constructs/v10 v10.2.55
 	github.com/aws/jsii-runtime-go v1.84.0
 	github.com/cdk8s-team/cdk8s-core-go/cdk8s/v2 v2.7.79
+	github.com/cdk8s-team/cdk8s-plus-go/cdk8splus24/v2 v2.8.41
 )
 
-require (
-	github.com/Masterminds/semver/v3 v3.2.1 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/yuin/goldmark v1.4.13 // indirect
-	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
-	golang.org/x/mod v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/tools v0.9.3 // indirect
-)
+require github.com/Masterminds/semver/v3 v3.2.1 // indirect
--- main.go
+++ main.go
@@ -1,37 +1,28 @@
 package main
 
 import (
+	"fmt"
+
+	"gihub.com/iuriikogan/perftest/imports/k8s"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 	"github.com/cdk8s-team/cdk8s-core-go/cdk8s/v2"
-	"github.com/iuriikogan/perftest/imports/k8s"
 )
 
-type PerfTestChartProps struct {
+type MyChartProps struct {
 	cdk8s.ChartProps
 }
 
-func NewPerfTestChart(scope constructs.Construct, id string, props *PerfTestChartProps) cdk8s.Chart {
+func NewMyChart(scope constructs.Construct, id string, props *MyChartProps) cdk8s.Chart {
 	var cprops cdk8s.ChartProps
 	if props != nil {
 		cprops = props.ChartProps
 	}
-	chart := cdk8s.NewChart(scope, jsii.String(id), &cprops)
+	chart := cdk8s.NewChart(scope, jsii.String("deployment"), &cprops)
 
-	label := map[string]*string{"app": jsii.String("nginx")}
+	label := map[string]*string{"app": jsii.String("hello-k8s")}
 
-	k8s.NewKubeService(chart, jsii.String("service"), &k8s.KubeServiceProps{
-		Spec: &k8s.ServiceSpec{
-			Type: jsii.String("LoadBalancer"),
-			Ports: &[]*k8s.ServicePort{{
-				Port:       jsii.Number(80),
-				TargetPort: k8s.IntOrString_FromNumber(jsii.Number(8080)),
-			}},
-			Selector: &label,
-		},
-	})
-
-	k8s.NewKubeDeployment(chart, jsii.String("deployment"), &k8s.KubeDeploymentProps{
+	obj := &k8s.KubeDeploymentProps{
 		Spec: &k8s.DeploymentSpec{
 			Replicas: jsii.Number(2),
 			Selector: &k8s.LabelSelector{
@@ -43,20 +34,24 @@ func NewPerfTestChart(scope constructs.Construct, id string, props *PerfTestChar
 				},
 				Spec: &k8s.PodSpec{
 					Containers: &[]*k8s.Container{{
-						Name:  jsii.String("nginx"),
-						Image: jsii.String("nginx:1.7.9"),
+						Name:  jsii.String("GoDeployment"),
+						Image: jsii.String("GoDeployment:1.0.0"),
 						Ports: &[]*k8s.ContainerPort{{ContainerPort: jsii.Number(8080)}},
 					}},
 				},
 			},
 		},
-	})
+	}
+
+	k8s.NewKubeDeployment(chart, jsii.String("goDeployment"), obj)
+
+	fmt.Println(*(*(obj.Spec.Template.Spec.Containers))[0].Image)
 
 	return chart
 }
 
 func main() {
 	app := cdk8s.NewApp(nil)
-	NewPerfTestChart(app, "nginx", nil)
+	NewMyChart(app, "cdk8sgo", nil)
 	app.Synth()
 }
